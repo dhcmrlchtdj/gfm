606214ee520a5e0eb4e7796830d3005c3f38bba8
niris <nirisix@gmail.com>
2017-07-11 15:34:08 +0800

    add peg

---
+++ Makefile
@@ -0,0 +1,5 @@
+default:
+	jbuilder build src/gfm.exe
+
+clean:
+	rm -rfv _build
---
+++ src/gfm.ml
@@ -0,0 +1,32 @@
+type peg = Peg.peg
+type src = Peg.src
+type res = Peg.res
+type mf = Peg.mf
+let print = Peg.print
+
+exception Err of string
+
+let rec start_list () (s:string) : res =
+    if String.length s = 0
+    then Ok (s, EOF)
+    else Error ("not match EOF")
+
+let rec block () (s:string) : res =
+    if String.length s = 0
+    then Ok (s, EOF)
+    else Error ("not match EOF")
+
+let rec doc : mf =
+    Peg.sequence [
+        start_list ();
+        Peg.star (block ())
+    ]
+
+let parse (s:string) : peg =
+    match doc s with
+        | Ok (_, p) -> p
+        | Error err -> raise (Err err)
+
+;;
+
+let input = "**bold**" in (parse input) |> print
