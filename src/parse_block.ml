3e762e57dc2b0bc1f0910f28f3d86726bed3e9c3
niris <nirisix@gmail.com>
2017-12-14 17:08:52 +0800

    update: split block

--- src/parse_block.ml
+++ src/parse_block.ml
@@ -10,19 +10,29 @@ type block =
     | UnorderedList of UTF8.t list
     | OrderedList of UTF8.t list
     | Paragraph of UTF8.t list
-    | ReferenceResolutionBlock
+    | ReferenceResolutionBlock of UTF8.t list
     | NullBlock
 
+let re_ref =
+    R.compile
+        "^ *\\[((((\\\\\\[)|(\\\\\\])|[^]])+)|(!\\[((\\\\\\[)|(\\\\\\])|[^]])+\\])|(!\\[((\\\\\\[)|(\\\\\\])|[^]])+\\]\\[((\\\\\\[)|(\\\\\\])|[^]])+\\]))\\] *: *([^ <>]+|<[^<>]*>)( .*)?$"
+
+
+let re_ref_end =
+    R.compile
+        "^ +((\"([^\"\\\\]|\\\\.)*\")|('([^'\\\\]|\\\\.)*')|(\\(([^(\\\\]|\\\\.)*\\))) *$"
+
+
 let re_horizontal =
-    R.regexp
+    R.compile
         "^ *((\\* *\\* *\\* *[\\* ]*)|(\\- *\\- *\\- *[\\- ]*)|(_ *_ *_ *[_ ]*))$"
 
 
-let re_unorder = R.regexp "^( *[-+*] +)[^ ]"
+let re_unorder = R.compile "^( *[-+*] +)[^ ]"
 
-let re_order = R.regexp "^( *[0-9]+\\. +)[^ ]"
+let re_order = R.compile "^( *[0-9]+\\. +)[^ ]"
 
-let re_header = R.regexp "^(-+|=+) *$"
+let re_header = R.compile "^(-+|=+) *$"
 
 let begin_with_four_space = String.starts_with "    "
 
@@ -33,6 +43,10 @@ let first_non_space (s: string) : char option =
 let include_non_space (len: int) (s: string) : bool =
     s |> String.enum |> Enum.take len
     |> Enum.exists (function ' ' -> false | _ -> true)
+
+
+let space_only (s: string) : bool =
+    s |> String.enum |> Enum.for_all (function ' ' -> true | _ -> false)
 
 
 let rec advance_code_block (acc: UTF8.t list) = function
@@ -89,46 +103,57 @@ let split_to_block (input: UTF8.t list) =
         | "" :: t ->
             let block = NullBlock in
             aux (block :: acc) t
-        | h :: t when not (begin_with_four_space h) && true ->
-            (* TODO *)
-            let block = ReferenceResolutionBlock in
-            aux (block :: acc) t
+        | h :: t when not (begin_with_four_space h) && re_ref =~ h -> (
+                let m = R.exec re_ref h in
+                match m with
+                    | Some mm ->
+                        let trailing_seq = mm.(18) in
+                        let b, tt =
+                            if space_only trailing_seq then
+                                match t with
+                                    | x :: (_ :: _ as y) when re_ref_end =~ x -> ([h; x], y)
+                                    | _ -> ([h], t)
+                            else ([h], t)
+                        in
+                        let block = ReferenceResolutionBlock b in
+                        aux (block :: acc) tt
+                    | _ -> failwith "never" )
         | h1 :: h2 :: t when re_header =~ h2 ->
             let block = SetexHeader (h1, h2) in
             aux (block :: acc) t
         | h :: t when String.starts_with "    " h ->
-            let b, t = advance_code_block [] (h :: t) in
+            let b, tt = advance_code_block [] (h :: t) in
             let block = CodeBlock (List.rev b) in
-            aux (block :: acc) t
+            aux (block :: acc) tt
         | h :: t when String.starts_with "#" h ->
             let block = AtxHeader h in
             aux (block :: acc) t
         | h :: t when first_non_space h = Some '>' ->
-            let b, t = advance_quote_block [] (h :: t) in
+            let b, tt = advance_quote_block [] (h :: t) in
             let block = BlockQuote (List.rev b) in
-            aux (block :: acc) t
+            aux (block :: acc) tt
         | h :: t when re_horizontal =~ h ->
             let block = HorizontalRule in
             aux (block :: acc) t
         | h :: t when re_unorder =~ h -> (
                 let m = R.exec re_unorder h in
                 match m with
-                    | Some [_; starter] ->
+                    | Some [|_; starter|] ->
                         let starter_len = String.length starter in
-                        let b, t =
+                        let b, tt =
                             advance_unordered_list starter starter_len [] (h :: t)
                         in
                         let block = UnorderedList (List.rev b) in
-                        aux (block :: acc) t
+                        aux (block :: acc) tt
                     | _ -> failwith "never" )
         | h :: t when re_order =~ h -> (
                 let m = R.exec re_order h in
                 match m with
-                    | Some [_; starter] ->
+                    | Some [|_; starter|] ->
                         let starter_len = String.length starter in
-                        let b, t = advance_ordered_list starter_len [] (h :: t) in
+                        let b, tt = advance_ordered_list starter_len [] (h :: t) in
                         let block = OrderedList (List.rev b) in
-                        aux (block :: acc) t
+                        aux (block :: acc) tt
                     | _ -> failwith "never" )
         | _ :: t ->
             (* TODO *)
--- src/r.ml
+++ src/r.ml
@@ -1,13 +1,13 @@
 open Batteries
 
-let regexp (pattern: string) : Re.re =
+let compile (pattern: string) : Re.re =
     pattern |> Re_perl.re ~opts:[] |> Re.compile
 
 
 let test (re: Re.re) (str: string) : bool = Re.execp re str
 
-let exec (re: Re.re) (str: string) : string list option =
-    Re.exec_opt re str |> Option.map Re.Group.all |> Option.map Array.to_list
+let exec (re: Re.re) (str: string) : string array option =
+    Re.exec_opt re str |> Option.map Re.Group.all
 
 
 module Infix = struct
