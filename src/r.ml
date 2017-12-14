58ef6a50a9b1826620fbb08c7abab3d6417cbdbc
niris <nirisix@gmail.com>
2017-12-14 14:04:55 +0800

    update: re signature

--- src/r.ml
+++ src/r.ml
@@ -1,8 +1,10 @@
 open Batteries
 
-let regexp (str: string) : Re.re = str |> Re_perl.re ~opts:[] |> Re.compile
+let regexp (pattern: string) : Re.re =
+    pattern |> Re_perl.re ~opts:[] |> Re.compile
 
-let test (re: Re.re) (str: string) = Re.execp re str
+
+let test (re: Re.re) (str: string) : bool = Re.execp re str
 
 let exec (re: Re.re) (str: string) : string list option =
     Re.exec_opt re str |> Option.map Re.Group.all |> Option.map Array.to_list
